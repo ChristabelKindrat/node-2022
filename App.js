cb7d96bddb383e2e3ddac95afad0d5aea56e534b
ChristabelKindrat <christabelkril@gmail.com>
2022-08-16 17:39:42 +0300

    Initial commit

---
+++ App.js
@@ -0,0 +1,28 @@
+// Home Work 1
+// const fs = require('fs/promises');
+// const path = require('path');
+//
+// const SortByGender = async (readFolder, gender, toFolder) => {
+//     try {
+//         const folder = path.join(__dirname, readFolder)
+//         const files = await fs.readdir(path.join(folder));
+//
+//         for (file of files) {
+//             const pathToFile = path.join(folder, file);
+//
+//             const data = await fs.readFile(pathToFile)
+//
+//             const user = JSON.parse(data.toString());
+//
+//             if (user.gender !== gender) {
+//                 await fs.rename(pathToFile, path.join(__dirname, toFolder, file));
+//             }
+//         }
+//     } catch (e) {
+//         console.log(e)
+//     }
+// }
+// SortByGender('boy', 'male', 'girl');
+// SortByGender('girl', 'female', 'boy');
+
+// Home Work 2
